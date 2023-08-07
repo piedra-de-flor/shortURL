959231d3219b0ba9f16b09f949c42e40cdc46b37
piedra-de-flor <xxspdlqjxx@naver.com>
2023-08-07 19:34:58 +0900

    Initial commit

---
+++ src/test/java/com/example/shortURL/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
